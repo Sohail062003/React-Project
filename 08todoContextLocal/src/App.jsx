b91da22098662ab705c228439349d8558b7449bb
Sohail Shaikh <sohailshaikh062003@gmail.com>
2025-09-08 23:56:52 +0530

    feat: implement UserContext and UserContextProvider, add Login and Profile components

--- 08todoContextLocal/src/App.jsx
+++ 08todoContextLocal/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <UserContextProvider>
       <h1 className='text-center bg-red-500'>hello </h1>
-    </>
+    </UserContextProvider>
   )
 }
 
---
+++ 08todoContextLocal/src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React, {useState, useContext} from 'react'
+import UserContext from '../context/UserContext'
+
+function Profile() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const {setUser} = useContext(UserContext);
+
+    const handleSubmit = () => {
+
+    }
+  return (
+    <div>
+      <h2>Login</h2>
+      <input type="text" 
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+       placeholder='username'
+     />
+      <input type="password" 
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder='password' 
+      />
+        <button onClick={handleSubmit}>Login</button>
+    </div>
+  )
+}
+
+export default Profile
